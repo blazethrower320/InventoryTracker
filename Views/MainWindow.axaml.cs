efe06e80684a9bd5ccedd48bf083e399c88b7fda
blazethrower320 <blazethrower320@gmail.com>
2025-08-07 00:59:31 -0500

    Add project files.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+using System;
+
+namespace InventoryTracker.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        /*
+         * ExtendClientAreaToDecorationsHint = true;
+        ExtendClientAreaChromeHints = Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome;
+        ExtendClientAreaTitleBarHeightHint = -1;
+         */
+    }
+
+}
